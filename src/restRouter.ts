31a956c1abbc9dcd98ad7ab5a55fae966f90a8cd
Damir <tebuchet@gmail.com>
2021-03-03 22:00:12 +0300

    ref

---
+++ src/restRouter.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const restRouter = express.Router();
+
+// restRouter.get('/test', (_, res) => {
+//   res.send('Hello World!');
+// });
+
+export default restRouter;
