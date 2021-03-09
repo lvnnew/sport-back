12517ed994dc88ec295c3c8eb8f49b2cab677e18
Damir <tebuchet@gmail.com>
2021-03-09 19:20:35 +0300

    types

--- src/agr/graph/services/tags/baseResolvers.ts
+++ src/agr/graph/services/tags/baseResolvers.ts
@@ -8,22 +8,23 @@ import {
   MutationUpdateTagArgs,
   MutationRemoveTagArgs,
 } from '../../../../generated/graphql';
+import {AgrContext} from '../../../services/context';
 
 const queryResolvers: Resolvers = {
   Query: {
     Tag: (_, {id}, {dataSources}) =>
       dataSources.tags.get(id),
-    allTags: (_, params: QueryAllTagsArgs, {dataSources}) =>
+    allTags: (_, params: QueryAllTagsArgs, {dataSources}: {dataSources: AgrContext}) =>
       dataSources.tags.all(params),
-    _allTagsMeta: (_, params: Query_AllTagsMetaArgs, {dataSources}) =>
+    _allTagsMeta: (_, params: Query_AllTagsMetaArgs, {dataSources}: {dataSources: AgrContext}) =>
       dataSources.tags.meta(params),
   },
   Mutation: {
-    createTag: (_, params: MutationCreateTagArgs, {dataSources}) =>
+    createTag: (_, params: MutationCreateTagArgs, {dataSources}: {dataSources: AgrContext}) =>
       dataSources.tags.create(params),
-    updateTag: (_, params: MutationUpdateTagArgs, {dataSources}) =>
+    updateTag: (_, params: MutationUpdateTagArgs, {dataSources}: {dataSources: AgrContext}) =>
       dataSources.tags.update(params),
-    removeTag: (_, params: MutationRemoveTagArgs, {dataSources}) =>
+    removeTag: (_, params: MutationRemoveTagArgs, {dataSources}: {dataSources: AgrContext}) =>
       dataSources.tags.delete(params),
   },
 };
