e4b0390e4b25266bd11e940ffa9c717c1e95318e
Damir <tebuchet@gmail.com>
2021-04-10 19:12:29 +0300

    deps

--- src/agr/elements.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable sort-keys-fix/sort-keys-fix */
-import {getAgrContext} from './services/context';
-// DO NOT EDIT! THIS IS GENERATED FILE
-
-
-export const agrElements = {
-  id: 'agr',
-  contextGetter: getAgrContext,
-};
