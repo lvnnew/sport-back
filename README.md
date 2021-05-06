bf4e5fef50907518eb321b4221b1260bc0df038b
Damir <tebuchet@gmail.com>
2021-05-06 14:55:41 +0300

    migration

---
+++ README.md
@@ -0,0 +1,4 @@
+
+```
+prisma migrate dev --preview-feature
+```
---
+++ prisma/migrations/20210506115521_wip/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Tag" (
+    "id" SERIAL NOT NULL,
+    "search" TEXT,
+    "comment" TEXT,
+
+    PRIMARY KEY ("id")
+);
