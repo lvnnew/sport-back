2966cd2a2c60978615a6599456560dd32c71d8c9
Damir <tebuchet@gmail.com>
2021-05-29 07:14:47 +0300

    deps

--- src/graph/typeDefs.ts
+++ src/graph/typeDefs.ts
@@ -2,9 +2,8 @@ import {mergeTypeDefs, loadFilesSync} from 'graphql-tools';
 import path from 'path';
 import {typeDefs} from 'graphql-scalars';
 
-const typesArray = loadFilesSync(path.join(__dirname, './services/*/typeDefs.?(ts)?(js)'));
 const typesArrayPrjcts = loadFilesSync(path.join(__dirname, '../*/graph/services/*/*TypeDefs.?(ts)?(js)'));
 
-const mergedTypeDefs = mergeTypeDefs([...typeDefs, ...typesArray, ...typesArrayPrjcts]);
+const mergedTypeDefs = mergeTypeDefs([...typeDefs, ...typesArrayPrjcts]);
 
 export default mergedTypeDefs;
