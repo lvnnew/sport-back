115928ade270b585659115da01f08f7cf96d3ecf
Damir <tebuchet@gmail.com>
2021-07-18 13:34:36 +0300

    cleaning

--- src/graph/gen/genData.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable sort-keys-fix/sort-keys-fix */
-/* eslint-disable import/no-commonjs */
-
-const generateData = require('data-generator-retail').default;
-
-export const genData = () => ({
-  ...(generateData({serializeDate: false}) as object),
-});
