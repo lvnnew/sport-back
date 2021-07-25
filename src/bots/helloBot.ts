faf1329be5ea378b7e313cf744cbd60179bbbbd7
Damir <tebuchet@gmail.com>
2021-07-25 18:48:42 +0300

    ref

---
+++ src/bots/helloBot.ts
@@ -0,0 +1,44 @@
+/* eslint-disable promise/prefer-await-to-callbacks */
+/* eslint-disable @typescript-eslint/camelcase */
+/* eslint-disable sort-keys-fix/sort-keys-fix */
+import {log} from '../log';
+import exitHook from 'exit-hook';
+import express, {Request, Response} from 'express';
+import {collectDefaultMetrics, register} from 'prom-client';
+import {closeCtx} from '../adm/services/context';
+import '../utils/polyfills/BigInt';
+
+// yarn src/bots/helloBot.ts
+
+exitHook(async () => {
+  await closeCtx();
+});
+
+const app = express();
+
+app.get('/health', (_: Request, res: Response) => {
+  res
+    .status(200)
+    .send({message: 'Ok'});
+});
+
+collectDefaultMetrics();
+
+app.get('/metrics', async (_req, res) => {
+  try {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+  } catch (error) {
+    res.status(500).end(error);
+  }
+});
+
+const startExpress = async () => {
+  const port = 3000;
+  app.listen({port}, () => {
+    log.info(`🚀 Server ready at http://localhost:${port}`);
+  });
+};
+
+startExpress()
+  .catch(error => log.error(error));
