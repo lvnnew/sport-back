44a08e6d0f6c1112a7e97e6e9131c912008aa2f4
Damir <tebuchet@gmail.com>
2021-10-06 01:29:39 +0300

    gen

--- src/init/permissions/initPermissions.ts
+++ src/init/permissions/initPermissions.ts
@@ -1,12 +1,19 @@
+/* eslint-disable no-use-before-define */
+import {initRoles} from './initRoles';
 import {Context} from '../../adm/services/context';
 import {getRuntimePermissions} from '../../adm/services/getRuntimePermissions';
 import {log} from '../../log';
 
-// yarn ts-node:withContext src/init/permissions/initPermissions.ts
-// AGR_DATABASE_URI=$AGR_STAGE_DATABASE_URI yarn ts-node:withContext src/init/permissions/initPermissions.ts
-// AGR_DATABASE_URI=$AGR_PROD_DATABASE_URI yarn ts-node:withContext src/init/permissions/initPermissions.ts
+// yarn init:permissions
+// AGR_DATABASE_URI=$AGR_STAGE_DATABASE_URI yarn init:permissions
+// AGR_DATABASE_URI=$AGR_PROD_DATABASE_URI yarn init:permissions
 
 export const initPermissions = async (ctx: Context) => {
+  await initPermissionsItself(ctx);
+  await initRoles(ctx);
+};
+
+export const initPermissionsItself = async (ctx: Context) => {
   const runtimePermissions = getRuntimePermissions();
 
   log.info(runtimePermissions.map(p => p.id));
