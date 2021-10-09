a559778d14c24edf0ced2748700fe225b7347588
Damir <tebuchet@gmail.com>
2021-10-09 05:20:22 +0300

    ref

--- src/workers/generalCronJobs.ts
+++ src/workers/generalCronJobs.ts
@@ -1,36 +1,6 @@
 import {CronItem} from 'graphile-worker';
 import {Job} from '../clients/queue/jobs/Job';
-
-export const getHourlyCronPattern = () => `${Math.floor(Math.random() * 59)} * * * *`;
-
-export const getFiveMinsCronPattern = () => '*/5 * * * *';
-
-export const getOneMinCronPattern = () => '* * * * *';
-
-export const constructCron = (
-  name: string,
-  identifier: string,
-  pattern: string,
-  queued = false,
-): CronItem => ({
-  task: name,
-  pattern,
-  options: {
-    queueName: queued ? name : undefined,
-    backfillPeriod: 0,
-  },
-  payload: {},
-  identifier,
-});
-
-export const hourlyCron = (name: string, queued = false): CronItem =>
-  constructCron(name, `${name}Hourly`, getHourlyCronPattern(), queued);
-
-export const fiveMinsCron = (name: string, queued = false): CronItem =>
-  constructCron(name, `${name}FiveMins`, getFiveMinsCronPattern(), queued);
-
-export const oneMinCron = (name: string, queued = false): CronItem =>
-  constructCron(name, `${name}OneMin`, getOneMinCronPattern(), queued);
+import {hourlyCron} from './utils';
 
 export const generalCronJobs: CronItem[] = [
   // hourly
--- src/workers/utils.ts
+++ src/workers/utils.ts
@@ -1,14 +1,50 @@
 import {getQueue} from '../clients/queue/getQueue';
-import {log} from '../log';
+import {CronItem} from 'graphile-worker';
 import {Job} from '../clients/queue/jobs/Job';
 import * as R from 'ramda';
+
+export const getHourlyCronPattern = () => `${Math.floor(Math.random() * 59)} * * * *`;
+
+export const getFiveMinsCronPattern = () => '*/5 * * * *';
+
+export const getOneMinCronPattern = () => '* * * * *';
+
+export const getSixHoursCronPattern = () => '0 */6 * * *';
+
+export const constructCron = (
+  name: string,
+  identifier: string,
+  pattern: string,
+  queued = false,
+): CronItem => ({
+  task: name,
+  pattern,
+  options: {
+    queueName: queued ? name : undefined,
+    backfillPeriod: 0,
+  },
+  payload: {},
+  identifier,
+});
+
+export const hourlyCron = (name: string, queued = false): CronItem =>
+  constructCron(name, `${name}Hourly`, getHourlyCronPattern(), queued);
+
+export const fiveMinsCron = (name: string, queued = false): CronItem =>
+  constructCron(name, `${name}FiveMins`, getFiveMinsCronPattern(), queued);
+
+export const oneMinCron = (name: string, queued = false): CronItem =>
+  constructCron(name, `${name}OneMin`, getOneMinCronPattern(), queued);
+
+export const onceInSixHoursCron = (name: string, queued = false): CronItem =>
+  constructCron(name, `${name}OnceInSixHours`, getSixHoursCronPattern(), queued);
 
 export const getQueueJobs = <T extends Record<string, any>>(jobs: T) => R.fromPairs(
   R.toPairs(
     jobs,
   ).map(
     ([jobName]) => [jobName, async (payload: any) => {
-      log.info(`jobName: ${jobName}`);
+      // log.info(`jobName: ${jobName}`);
       const queue = await getQueue();
 
       await queue.addJob(jobName as unknown as Job, payload);
