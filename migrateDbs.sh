ff9aff632f61bc8eaadb5bad0406574c646faaf3
Damir <tebuchet@gmail.com>
2021-12-06 23:16:41 +0300

    "ci"

--- migrateDbs.sh
+++ migrateDbs.sh
@@ -3,11 +3,9 @@
 set -e
 set -v
 
-DATABASE_URI=$AGR_DATABASE_URI npx prisma migrate deploy --preview-feature
+DATABASE_URI=$AGR_DEV_DATABASE_URI npx prisma migrate deploy --preview-feature
 
 DATABASE_URI=$AGR_TEST_DATABASE_URI npx prisma migrate deploy --preview-feature
 DATABASE_URI=$AGR_STAGE_DATABASE_URI npx prisma migrate deploy --preview-feature
-
-DATABASE_URI=$AGR_DEV_KARIMOVA_DATABASE_URI npx prisma migrate deploy --preview-feature
 
 DATABASE_URI=$AGR_PROD_DATABASE_URI npx prisma migrate deploy --preview-feature
