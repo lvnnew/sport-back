02e6f8afff9de2af6d8226d09b4ea6d7355232c4
Damir <tebuchet@gmail.com>
2022-01-15 21:09:04 +0300

    "gen"

--- src/init/comon/initAuditLogActionTypes.ts
+++ src/init/comon/initAuditLogActionTypes.ts
@@ -3,9 +3,11 @@ import AuditLogActionType from '../../types/AuditLogActionType';
 
 // DO NOT EDIT! THIS IS GENERATED FILE
 
-export const initAuditLogActionTypes = async (ctx: Context) => {
+const initAuditLogActionTypes = async (ctx: Context) => {
   await ctx.auditLogActionTypes.upsert({
     id: AuditLogActionType.NewEntityData,
     title: 'Новые данные сущности',
   });
 };
+
+export default initAuditLogActionTypes;
