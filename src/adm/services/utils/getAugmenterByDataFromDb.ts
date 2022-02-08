f2218f78c74da95f3b6c22513ccd94088828e0bc
Damir <tebuchet@gmail.com>
2022-02-08 22:55:04 +0300

    "gen"

--- src/adm/services/utils/getAugmenterByDataFromDb.ts
+++ src/adm/services/utils/getAugmenterByDataFromDb.ts
@@ -6,17 +6,23 @@ const getAugmenterByDataFromDb = <ID extends string | bigint | number, T extends
   augmentedFields: string[],
 ) => async (
     data: PartialMaybe<T> & {id: ID},
-  ): Promise<T> => ({
-    ...(
-      await getter({
-        where: {id: data.id},
-        select: R.fromPairs(augmentedFields.map(f => [f, true])),
-      })
-    ),
-    ...R.omit(
-      augmentedFields,
-      data,
-    ),
-  }) as unknown as T;
+  ): Promise<T> => {
+    if (augmentedFields.length === 0) {
+      return data as unknown as T;
+    }
+
+    return {
+      ...(
+        await getter({
+          where: {id: data.id},
+          select: R.fromPairs(augmentedFields.map(f => [f, true])),
+        })
+      ),
+      ...R.omit(
+        augmentedFields,
+        data,
+      ),
+    } as unknown as T;
+  };
 
 export default getAugmenterByDataFromDb;
