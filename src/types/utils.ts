4b481503a1a633f5b2dea0d4a93887e4abeee67a
Damir <tebuchet@gmail.com>
2022-03-10 13:22:57 +0300

    "fix"

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,6 +1,12 @@
 export type ThenArg<T> = T extends PromiseLike<infer U> ? U : T;
 
 export type PartialMaybe<T> = {
-    [P in keyof T]?: T[P] | null;
-  };
+  [P in keyof T]?: T[P] | null;
+};
+
+export type NonNullableRecord<T> = {
+  [P in keyof T]: NonNullable<T[P]>;
+};
+
+export type DefinedRecord<T> = NonNullableRecord<Required<T>>;
 
