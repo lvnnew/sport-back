9e34e59d6f15e0e1ac12ab103427a16b65e717b2
Damir <tebuchet@gmail.com>
2022-04-07 16:20:04 +0300

    "deps"

--- src/adm/services/getHooksUtils.ts
+++ src/adm/services/getHooksUtils.ts
@@ -12,7 +12,7 @@ export type HooksUtilsType<
   SMU extends {} = MU, // strict mutation create entity arguments
   > = {
   changeListFilter: (ctx: Context, args: QA) => Promise<QA>;
-  beforeCreate: (ctx: Context, data: SMC) => Promise<SMC>;
+  beforeCreate: (ctx: Context, data: MC) => Promise<MC>;
   afterCreate: (ctx: Context, data: E) => Promise<void>;
   beforeUpdate: (ctx: Context, data: SMU) => Promise<SMU>;
   afterUpdate: (ctx: Context, data: E) => Promise<void>,
