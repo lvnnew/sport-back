6a684fa6c9d4c8ba6b7ab824b83c7913e2939ea2
Damir <tebuchet@gmail.com>
2022-04-09 15:43:27 +0300

    "gen"

--- src/rest/healthRouter.ts
+++ src/rest/healthRouter.ts
@@ -3,9 +3,11 @@ import expressRateLimit from 'express-rate-limit';
 import {createContext} from '../adm/services/context';
 import log from '../log';
 
+const rateLimitWindow = 5;
+const rateLimitPerIp = 10;
 const limiter = expressRateLimit({
-  windowMs: 5 * 1000, // 5 sec
-  max: 5 * 5, // Limit each IP to 25 requests per 'window' (here, per 5 sec)
+  windowMs: rateLimitWindow * 1000, // 5 sec
+  max: rateLimitWindow * rateLimitPerIp, // Limit each IP to 25 requests per 'window' (here, per 5 sec)
   standardHeaders: true, // Return rate limit info in the 'RateLimit-*' headers
   legacyHeaders: false, // Disable the 'X-RateLimit-*' headers
 });
@@ -14,14 +16,15 @@ const healthRouter: Router = express.Router();
 
 healthRouter.use(limiter);
 
-healthRouter.get('/', async (_: Request, res: Response) => {
-  log.info('health');
+healthRouter.get('/', async (req: Request, res: Response) => {
+  const startDate = Date.now();
+  log.info(`health, type: "${req.query.type}", remote id: "${req.headers['x-real-ip'] || req.connection.remoteAddress}"`);
   const ctx = await createContext();
 
   // check by Knex
   const knexResult = await ctx.knex.raw('SELECT 1 as check');
-  log.info('knexResult');
-  log.info(JSON.stringify(knexResult.rows[0].check));
+  // log.info('knexResult');
+  // log.info(JSON.stringify(knexResult.rows[0].check));
   if (!knexResult || !knexResult.rows || knexResult.rows.length !== 1 || knexResult.rows[0].check !== 1) {
     log.error('Can not request bd by knex');
     res
@@ -32,8 +35,8 @@ healthRouter.get('/', async (_: Request, res: Response) => {
 
   // check by prisma
   const prismaResult = await ctx.prisma.manager.count();
-  log.info('prismaResult');
-  log.info(`prismaResult: ${prismaResult}`);
+  // log.info('prismaResult');
+  // log.info(`prismaResult: ${prismaResult}`);
   if (!(prismaResult >= 0)) {
     log.error('Can not request bd by prisma');
     res
@@ -41,6 +44,8 @@ healthRouter.get('/', async (_: Request, res: Response) => {
       .send({message: 'Can not request bd by prisma'});
     return;
   }
+
+  log.info(`Health check finshed in ${(Date.now() - startDate) / 1000} sec`);
 
   res
     .status(200)
