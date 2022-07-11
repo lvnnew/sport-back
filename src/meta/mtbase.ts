4aa85628b468ebe54481581daf14d15b0e58d9d4
ann <anna.laznia@gmail.com>
2022-07-11 14:35:00 +0300

    meta data in project

---
+++ src/meta/mtbase.ts
@@ -0,0 +1,56 @@
+import jetpack from 'fs-jetpack';
+import {
+  BootstrapEntityOptions,
+  defaultBootstrapEntityOptions,
+  generateProject,
+  SystemMetaBuilder,
+  addCommonEntities,
+} from 'runlify';
+import path from 'path';
+
+// yarn ts-node src/meta/mtbase.ts
+
+const dir = jetpack.cwd('..').cwd();
+
+const opts: BootstrapEntityOptions = {
+  ...defaultBootstrapEntityOptions,
+  genUiCountWidget: true,
+  genUiDashboard: true,
+  genUiElements: true,
+  genUiEntityMapping: true,
+  genUiFunctions: true,
+  genUiListWidget: true,
+  genUiMenu: true,
+  genUiResources: true,
+  genUiResourcesPage: true,
+  genUiRoutes: true,
+  skipWarningThisIsGenerated: false,
+
+  detachedBackProject: path.join(dir, 'mtbase-back'),
+  detachedUiProject: path.join(dir, 'mtbase-ui'),
+  projectsGroup: 'mtbase',
+  dbName: 'mtbase_stage',
+  projectName: 'Meta Template Base',
+};
+
+const system = new SystemMetaBuilder('mtbase', opts);
+
+system.addConfigVar('smtp.host', '', 'Хост почтового сервера');
+system.addConfigVar('smtp.port', '', 'Порт почтового сервера');
+system.addConfigVar('smtp.user', '', 'Имя пользователя для авторизации на почтовом сервере');
+system.addConfigVar('smtp.pass', '', 'Пароль пользователя для авторизации на почтовом сервере');
+system.addConfigVar('smtp.from', '', 'Почтовый адрес, от имени которого следует отправлять письма');
+
+addCommonEntities(system);
+
+// stats
+const stats = system.addCatalog('stats', 'Stats', opts);
+stats.setNeedFor('Статистика');
+stats.getKey().setType('string');
+stats.addField('updated').setType('datetime');
+stats.addField('helloCount').setType('int');
+
+const tags = system.addCatalog('tags', 'Tags', opts);
+tags.addField('comment').setType('string');
+
+generateProject(system.build(), opts);
