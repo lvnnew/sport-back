636990114246ba71719c2afcf296ced97e3afcea
Damir <tebuchet@gmail.com>
2022-11-30 00:55:33 +0300

    initPermissions

--- initPermissions.sh
+++ initPermissions.sh
@@ -5,10 +5,13 @@ set -v
 
 yarn global add runlify
 
-# initRoles
+filenames=$(ls ./config -I 'default.json' -I 'migration.json' -I '*copy*' -I '*.json_')
+echo $filenames
 
-runlify start yarn ts-node:withContext src/init/permissions/initRoles.ts
-runlify start env=test yarn ts-node:withContext src/init/permissions/initRoles.ts
-runlify start env=stage yarn ts-node:withContext src/init/permissions/initRoles.ts
-
-runlify start env=prod yarn ts-node:withContext src/init/permissions/initRoles.ts
+for filename in $filenames; do
+  env="${filename//.json/}"
+  echo env: $env
+  command="yarn runlify start env=$env yarn ts-node:withContext src/init/permissions/initRoles.ts"
+  echo command: $command
+  $command
+done
