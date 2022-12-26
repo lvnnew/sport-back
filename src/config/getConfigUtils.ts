72508a432648df72807c6c992d69f9e93c31cd2d
Damir <tebuchet@gmail.com>
2022-12-27 01:05:06 +0300

    gen

---
+++ src/config/getConfigUtils.ts
@@ -0,0 +1,43 @@
+const getConfigUtils = (getFromNconf: (name: string, required: boolean) => string | undefined) => {
+  const getStringConfig = (name: string, required: boolean) => getFromNconf(name, required);
+
+  const getIntConfig = (name: string, required: boolean) => {
+    const value = getFromNconf(name, required);
+
+    return (value ? Number(value) : undefined);
+  };
+
+  const getFloatConfig = getIntConfig;
+
+  const getBigIntConfig = (name: string, required: boolean) => {
+    const value = getFromNconf(name, required);
+
+    return (value ? BigInt(value) : undefined);
+  };
+
+  const getDateTimeConfig = (name: string, required: boolean) => {
+    const value = getFromNconf(name, required);
+
+    return (value ? new Date(value) : undefined);
+  };
+
+  const getDateConfig = getDateTimeConfig;
+
+  const getBooleanConfig = (name: string, required: boolean) => {
+    const value = getFromNconf(name, required);
+
+    return (value ? Boolean(value) : undefined);
+  };
+
+  return {
+    getStringConfig,
+    getIntConfig,
+    getFloatConfig,
+    getBigIntConfig,
+    getDateTimeConfig,
+    getDateConfig,
+    getBooleanConfig,
+  };
+};
+
+export default getConfigUtils;
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,7 @@
 import {constantCase} from 'change-case';
 import nconf from 'nconf';
 import {exists, read} from 'fs-jetpack';
+import getConfigUtils from './getConfigUtils';
 
 // DO NOT EDIT! THIS IS GENERATED FILE
 
@@ -20,45 +21,58 @@ if (exists(file)) {
 
 export const isLocalEnv = envName === 'local';
 
-export const getFromNconf = (name: string): string | undefined => nconf.get(constantCase(name)) || nconf.get(name) || '';
+type ValueBasedOnRequired<Req extends boolean, Val extends number | string | boolean | Date | bigint>
+  = Req extends true ? Val : Val | undefined
+
+export const getFromNconf = <T extends boolean>(name: string, required: T): ValueBasedOnRequired<T, string> => {
+  const value = nconf.get(constantCase(name)) || nconf.get(name);
+
+  if (required && value === undefined) {
+    throw new Error(`Config var "${name}" is required`);
+  }
+
+  return value;
+};
+
+const utils = getConfigUtils(getFromNconf);
 
 const envConfig = {
-  admJwtSecret: getFromNconf('adm.jwt.secret'),
-  adminRecaptchaRequiredScore: getFromNconf('admin.recaptcha.requiredScore'),
-  adminRecaptchaSecretKey: getFromNconf('admin.recaptcha.secretKey'),
-  appEnvironment: getFromNconf('app.environment'),
-  appJwtSecret: getFromNconf('app.jwt.secret'),
-  appName: getFromNconf('app.name'),
-  appTitle: getFromNconf('app.title'),
-  customerRecaptchaRequiredScore: getFromNconf('customer.recaptcha.requiredScore'),
-  customerRecaptchaSecretKey: getFromNconf('customer.recaptcha.secretKey'),
-  databaseUri: getFromNconf('database.uri'),
-  esCloudId: getFromNconf('es.cloudId'),
-  esNode: getFromNconf('es.node'),
-  esPassword: getFromNconf('es.password'),
-  esTlsRejectUnauthorized: getFromNconf('es.tls.rejectUnauthorized'),
-  esUsername: getFromNconf('es.username'),
-  graphqlPlaygroundEnabled: getFromNconf('graphql.playground.enabled'),
-  kafkaBrokers: getFromNconf('kafka.brokers'),
-  kafkaEnabled: getFromNconf('kafka.enabled'),
-  kafkaPassword: getFromNconf('kafka.password'),
-  kafkaQueueAcks: getFromNconf('kafka.queue.acks'),
-  kafkaQueueDefaultRetryTime: getFromNconf('kafka.queue.defaultRetryTime'),
-  kafkaQueueMaxAttemptsSize: getFromNconf('kafka.queue.maxAttemptsSize'),
-  kafkaQueueStackSize: getFromNconf('kafka.queue.stackSize'),
-  kafkaQueueSupportedVersion: getFromNconf('kafka.queue.supportedVersion'),
-  kafkaQueueWaitingInterruptTime: getFromNconf('kafka.queue.waitingInterruptTime'),
-  kafkaSslRejectUnauthorized: getFromNconf('kafka.ssl.rejectUnauthorized'),
-  kafkaUsername: getFromNconf('kafka.username'),
-  logsFormat: getFromNconf('logs.format'),
-  lokiUrl: getFromNconf('loki.url'),
-  s3AccessKeyId: getFromNconf('s3.accessKeyId'),
-  s3SecretAccessKey: getFromNconf('s3.secretAccessKey'),
-  smtpFrom: getFromNconf('smtp.from'),
-  smtpHost: getFromNconf('smtp.host'),
-  smtpPass: getFromNconf('smtp.pass'),
-  smtpPort: getFromNconf('smtp.port'),
-  smtpUser: getFromNconf('smtp.user'),
+  admJwtSecret: utils.getStringConfig('adm.jwt.secret', true),
+  adminRecaptchaRequiredScore: utils.getFloatConfig('admin.recaptcha.requiredScore', true),
+  adminRecaptchaSecretKey: utils.getStringConfig('admin.recaptcha.secretKey', true),
+  appEnvironment: utils.getStringConfig('app.environment', true),
+  appJwtSecret: utils.getStringConfig('app.jwt.secret', true),
+  appName: utils.getStringConfig('app.name', true),
+  appTitle: utils.getStringConfig('app.title', true),
+  customerRecaptchaRequiredScore: utils.getStringConfig('customer.recaptcha.requiredScore', true),
+  customerRecaptchaSecretKey: utils.getStringConfig('customer.recaptcha.secretKey', true),
+  databaseUri: utils.getStringConfig('database.uri', true),
+  esCloudId: utils.getStringConfig('es.cloudId', true),
+  esNode: utils.getStringConfig('es.node', true),
+  esPassword: utils.getStringConfig('es.password', true),
+  esTlsRejectUnauthorized: utils.getBooleanConfig('es.tls.rejectUnauthorized', true),
+  esUsername: utils.getStringConfig('es.username', true),
+  graphqlPlaygroundEnabled: utils.getBooleanConfig('graphql.playground.enabled', true),
+  kafkaBrokers: utils.getStringConfig('kafka.brokers', true),
+  kafkaEnabled: utils.getBooleanConfig('kafka.enabled', true),
+  kafkaPassword: utils.getStringConfig('kafka.password', true),
+  kafkaQueueAcks: utils.getIntConfig('kafka.queue.acks', true),
+  kafkaQueueDefaultRetryTime: utils.getIntConfig('kafka.queue.defaultRetryTime', true),
+  kafkaQueueMaxAttemptsSize: utils.getIntConfig('kafka.queue.maxAttemptsSize', true),
+  kafkaQueueStackSize: utils.getIntConfig('kafka.queue.stackSize', true),
+  kafkaQueueSupportedVersion: utils.getStringConfig('kafka.queue.supportedVersion', true),
+  kafkaQueueWaitingInterruptTime: utils.getIntConfig('kafka.queue.waitingInterruptTime', true),
+  kafkaSslRejectUnauthorized: utils.getBooleanConfig('kafka.ssl.rejectUnauthorized', true),
+  kafkaUsername: utils.getStringConfig('kafka.username', true),
+  logsFormat: utils.getStringConfig('logs.format', true),
+  lokiUrl: utils.getStringConfig('loki.url', true),
+  s3AccessKeyId: utils.getStringConfig('s3.accessKeyId', true),
+  s3SecretAccessKey: utils.getStringConfig('s3.secretAccessKey', true),
+  smtpFrom: utils.getStringConfig('smtp.from', ),
+  smtpHost: utils.getStringConfig('smtp.host', ),
+  smtpPass: utils.getStringConfig('smtp.pass', ),
+  smtpPort: utils.getIntConfig('smtp.port', ),
+  smtpUser: utils.getStringConfig('smtp.user', ),
 };
 
 export type Config = typeof envConfig;
