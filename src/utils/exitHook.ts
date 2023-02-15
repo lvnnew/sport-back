1d29f465fab4ca71bba31f2214693fc298a910ea
Damir <tebuchet@gmail.com>
2023-02-15 23:11:42 +0300

    fix

--- src/utils/exitHook.ts
+++ src/utils/exitHook.ts
@@ -1,7 +1,15 @@
 import Graceful, {GracefulListener} from 'node-graceful';
+import log from '../log';
 
 Graceful.captureRejections = true;
 Graceful.captureExceptions = true;
 Graceful.exitOnDouble = false;
+
+Graceful.on('exit', (event, error) => {
+  log.info(`Exit with event: ${event}`);
+  if (error) {
+    log.error(error);
+  }
+});
 
 export const exitHook = (onExit: GracefulListener) => Graceful.on('exit', onExit);
