5a1b3ac0e903e88cac8780ce3e8e11881e552683
tebuchet <tebuchet@gmail.com>
2023-03-02 23:21:40 +0300

    bucketInit

--- src/init/initS3Buckets.ts
+++ src/init/initS3Buckets.ts
@@ -4,8 +4,8 @@ import {getConfig} from '../config';
 const initS3Buckets = async () => {
   const {s3BucketEmailFiles, s3BucketTmpFilesToDownload} = await getConfig();
 
-  createS3BucketIfNotExist(s3BucketEmailFiles, true);
-  createS3BucketIfNotExist(s3BucketTmpFilesToDownload, true);
+  await createS3BucketIfNotExist(s3BucketEmailFiles, true);
+  await createS3BucketIfNotExist(s3BucketTmpFilesToDownload, true);
 };
 
 export default initS3Buckets;
