bf56f076d7ade6ff30283debff7d224b6f879d64
Damir <tebuchet@gmail.com>
2023-08-20 10:26:28 +0300

    gen

---
+++ src/init/elastic/genJobs.ts
@@ -0,0 +1,9 @@
+import Entity from '../../types/Entity';
+import {textFields, keywordFields, integerFields, dateFields, booleanFields} from './utils';
+import {ElasticJobs} from './type';
+
+// DO NOT EDIT! THIS IS GENERATED FILE
+
+export const genJobs: ElasticJobs = {
+  
+};
